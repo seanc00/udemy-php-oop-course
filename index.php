970883e54058c9b31b1f2dcf6a73a310abbd56e3
Sean Connolly <sc@maxwebsolutions.com>
2026-07-07 12:57:51 +0100

    Completed lessons up to 173

--- index.php
+++ index.php
@@ -1,12 +1,26 @@
-<?php include("includes/header.php"); ?>
+<?php
+    include("includes/header.php");
+
+    $photos = Photo::findAll();
+?>
 
 <div class="row">
     <!-- Blog Entries Column -->
-    <div class="col-md-8"></div>
+    <div class="col-md-12">
+        <div class="thumbnails row">
+            <?php foreach ($photos as $key => $photo): ?>
+                <div class="col-xs-6 col-md-3">
+                    <a class="thumbnail" href="">
+                        <img src="admin/<?= $photo->picture_path(); ?>" alt="">
+                    </a>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
 
     <!-- Blog Sidebar Widgets Column -->
-    <div class="col-md-4">
-    <?php include("includes/sidebar.php"); ?>
+    <!-- <div class="col-md-4"> -->
+    <?php // include("includes/sidebar.php"); ?>
 </div>
 <!-- /.row -->
 
